04b44c62493ba301a9ad161e468cdada77cf83a0
Alan <62050854+alannan55@users.noreply.github.com>
2020-03-19 20:33:42 +1100

    Add files via upload

--- pname.c
+++ pname.c
@@ -1,5 +1,5 @@
 /*
- * src/tutorial/pname.c
+ * src/tutorial/complex.c
  *
  ******************************************************************************
   This file contains routines that can be bound to a Postgres backend and
@@ -8,30 +8,36 @@
 ******************************************************************************/
 
 #include "postgres.h"
-
+#include<string.h>
 #include "fmgr.h"
-#include "libpq/pqformat.h"			/* needed for send/recv functions */
-#include <ctype.h>
-#include <string.h>
+#include "libpq/pqformat.h"		/* needed for send/recv functions */
 #include "access/hash.h"
+
+//check
+//> < = 
+//show
 
 PG_MODULE_MAGIC;
 
 typedef struct person
 {
-	int     i;
-	char	person_name[1];
+	int length;
+	char person_name[1];
 }PersonName;
 
 int check_input(char *string);
-int person_name_compare(PersonName *x, PersonName *y);
+int person_name_compare(PersonName *a, PersonName *b);
+/*****************************************************************************
+ * Input/Output functions
+ *****************************************************************************/
+
+PG_FUNCTION_INFO_V1(pname_in);
 
 int check_input(char *string){
 
 	int len = strlen(string);
-	int comma = 0;
-	int index = 0;
 	int comma_number = 0;
+	int index = 0;
 	char *title_a = "Dr";
 	char *title_b = "Prof";
 	char *title_c = "Mr";
@@ -41,7 +47,7 @@ int check_input(char *string){
 	// get the index of comma in the string
 	for(int i = 0; i < len; i++){
 		if(string[i] == ','){
-			comma++;
+			comma_number++;
 			index = i;
 		}
 		if(isdigit(string[i])){
@@ -100,20 +106,13 @@ int check_input(char *string){
 	return 1;
 }
 
-/*****************************************************************************
- * Input/Output functions
- *****************************************************************************/
-
-PG_FUNCTION_INFO_V1(pname_in);
-
 Datum
 pname_in(PG_FUNCTION_ARGS)
 {
-	char	   *str = PG_GETARG_CSTRING(0);
+	char *str = PG_GETARG_CSTRING(0);
 	PersonName *result;
+	int length = strlen(str) + 1;
 
-	int len = strlen(str);
-	// check valid person name format
 	if(check_input(str) == 0){
 		ereport(ERROR,
 				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
@@ -121,10 +120,11 @@ pname_in(PG_FUNCTION_ARGS)
 						"pname", str)));
 	}
 
-	result = (PersonName *) palloc(sizeof(len + 1 +VARHDRSZ));
-	SET_VARSIZE(result , len + 1 +VARHDRSZ);
-	// assign the value of str to the result
-	snprintf(result->person_name,len+1,"%s",str);
+	result = (PersonName *) palloc(VARHDRSZ + length);
+
+	SET_VARSIZE(result, VARHDRSZ + length);
+	snprintf(result->person_name, length, "%s", str);
+
 	PG_RETURN_POINTER(result);
 }
 
@@ -133,10 +133,11 @@ PG_FUNCTION_INFO_V1(pname_out);
 Datum
 pname_out(PG_FUNCTION_ARGS)
 {
-	PersonName    *pName = (PersonName *) PG_GETARG_POINTER(0);
+	// select name from Students;
+	PersonName    *personName = (PersonName *) PG_GETARG_POINTER(0);
 	char	   *result;
-	
-	result = psprintf("$s", pName->person_name);
+
+	result = psprintf("%s", personName->person_name);
 	PG_RETURN_CSTRING(result);
 }
 
@@ -146,236 +147,283 @@ pname_out(PG_FUNCTION_ARGS)
  * These are optional.
  *****************************************************************************/
 
-/*****************************************************************************
 PG_FUNCTION_INFO_V1(pname_recv);
+
 Datum
 pname_recv(PG_FUNCTION_ARGS)
 {
 	StringInfo	buf = (StringInfo) PG_GETARG_POINTER(0);
 	PersonName    *result;
-	char *pName = pq_getmsgstring(buf);
-	int len = strlen(pName);
-	result = (PersonName *) palloc(VARHDRSZ + len + 1);
-	SET_VARSIZE(result,VARHDRSZ + len + 1)
-	snprintf(result->person_name,len+1,"%s",pName)
+
+	const char *person_name = pq_getmsgstring(buf);
+	int length = strlen(person_name) + 1;
+	
+	result = (PersonName *) palloc(VARHDRSZ + length);
+	SET_VARSIZE(result, VARHDRSZ + length);
+	snprintf(result->person_name, length, "%s", person_name);
 	PG_RETURN_POINTER(result);
 }
+
 PG_FUNCTION_INFO_V1(pname_send);
+
 Datum
 pname_send(PG_FUNCTION_ARGS)
 {
-	PersonName    *pName = (PersonName *) PG_GETARG_POINTER(0);
+	PersonName    *personName = (PersonName *) PG_GETARG_POINTER(0);
+
 	StringInfoData buf;
 	pq_begintypsend(&buf);
-	pq_sendstring(&buf, pName->person_name);
+
+	pq_sendstring(&buf, personName->person_name);
+
 	PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
 }
-*****************************************************************************/
 
 /*****************************************************************************
- * New Operators
+ * Operator class for defining B-tree index
  *
- * person_name_less_than
- * person_name_less_than_or_equal
- * person_name_equal
- * person_name_greater_than_or_equal
- * person_name_greater_than
- * 
+ * It's essential that the comparison operators and support function for a
+ * B-tree index opclass always agree on the relative ordering of any two
+ * data values.  Experience has shown that it's depressingly easy to write
+ * unintentionally inconsistent functions.  One way to reduce the odds of
+ * making a mistake is to make all the functions simple wrappers around
+ * an internal three-way-comparison function, as we do here.
  *****************************************************************************/
 
-// compare two person names
-// equal: return 0
-// else: return strcmp result
-int person_name_compare(PersonName *x, PersonName *y){
+// #define Mag(c)	((c)->x*(c)->x + (c)->y*(c)->y)
 
-	int len_x = strlen(x->person_name);
-	int len_y = strlen(y->person_name);
+// static int
+// complex_abs_cmp_internal(Complex * a, Complex * b)
+// {
+// 	double		amag = Mag(a),
+// 				bmag = Mag(b);
 
-	char *person_name_x = x->person_name;
-	char *person_name_y = y->person_name;
+// 	if (amag < bmag)
+// 		return -1;
+// 	if (amag > bmag)
+// 		return 1;
+// 	return 0;
+// }
 
-	char *family_name_x;
-	char *family_name_y;
-	char *given_name_x;
-	char *given_name_y;
+int person_name_compare(PersonName *a, PersonName *b){
 
-	int family_name_result;
-	int given_name_result;
-
-	int index_x = 0;
-	int index_y = 0;
-
-	// get the index of comma of the person name x
-	for(int i = 0; i < len_x; i++){
-		if(x->person_name[i] == ','){
-			index_x = i;
-			break;
-		}
-	}
-	// get the index of comma of the person name y
-	for(int j = 0; j < len_y; j++){
-		if(y->person_name[j] == ','){
-			index_y = j;
+	int a_index = 0, b_index = 0;
+	char * a_given_name, b_given_name;
+	int result;
+	for (a_index=0;a_index<strlen(a->person_name);a_index++){
+		if(a->person_name[a_index] == ','){
 			break;
 		}
 	}
 
-
-	// get the family name of x and y
-	strncpy(family_name_x, person_name_x, index_x);
-	strncpy(family_name_y, person_name_y, index_y);
-
-	family_name_result = strcmp(family_name_x,family_name_y);
-
-	// get the given name of x and y
-	strncpy(given_name_x, person_name_x + index_x + 1, len_x - index_x - 1);
-	strncpy(given_name_y, person_name_y + index_y + 1, len_y - index_y - 1);
-
-	// judge the family name
-	if(family_name_result != 0){
-		return family_name_result;
+	for (b_index=0;b_index<strlen(b->person_name);b_index++){
+		if(b->person_name[b_index] == ','){
+			break;
+		}
 	}
 
-	// if there is a space after comma
-	if(x->person_name[index_x + 1] == ' '){
-		given_name_x++;
-	}
-	if(y->person_name[index_y + 1] == ' '){
-		given_name_y++;
-	}
+	// char * a_given_name = strchr(a->pname, ',');
+	// a_index = strlen(a->pname) - strlen(a_given_name);
+	// char * b_given_name = strchr(a->pname, ',');
+	// b_index = strlen(b->pname) - strlen(b_given_name);
 
-	given_name_result = strcmp(given_name_x,given_name_y);
 
-	// judge the given name
-	if(given_name_result != 0){
-		return given_name_result;
+	a->person_name[a_index] = '\0';
+	b->person_name[b_index] = '\0';
+	result = strcmp(a->person_name, b->person_name);
+	a->person_name[a_index] = ',';
+	b->person_name[b_index] = ',';
+	a_given_name = &(a->person_name[a_index]);
+	b_given_name = &(b->person_name[b_index]);
+
+	if(result == 0){
+		if (a->person_name[a_index + 1] == ' '){
+			a_given_name++;
+		}
+		if (b->person_name[b_index + 1] == ' '){
+			b_given_name++;
+		}
+
+		result = strcmp(a_given_name, b_given_name);
 	}
-	return 0;
+	return result;
 }
+
 
 PG_FUNCTION_INFO_V1(person_name_less_than);
 
 Datum
-// less than: person name x < person name y
 person_name_less_than(PG_FUNCTION_ARGS)
-{ 
-	PersonName    *x = (PersonName *) PG_GETARG_POINTER(0);
-	PersonName    *y = (PersonName *) PG_GETARG_POINTER(1);
+{
+	PersonName    *a = (PersonName *) PG_GETARG_POINTER(0);
+	PersonName    *b = (PersonName *) PG_GETARG_POINTER(1);
 
-	PG_RETURN_BOOL(person_name_compare(x, y) < 0);
+	PG_RETURN_BOOL(person_name_compare(a,b) < 0);
 }
 
 PG_FUNCTION_INFO_V1(peson_name_less_than_or_equal);
 
 Datum
-// less than or equal: person name x <= person name y
 peson_name_less_than_or_equal(PG_FUNCTION_ARGS)
 {
-	PersonName    *x = (PersonName *) PG_GETARG_POINTER(0);
-	PersonName    *y = (PersonName *) PG_GETARG_POINTER(1);
+	PersonName    *a = (PersonName *) PG_GETARG_POINTER(0);
+	PersonName    *b = (PersonName *) PG_GETARG_POINTER(1);
 
-	PG_RETURN_BOOL(person_name_compare(x, y) <= 0);
+	PG_RETURN_BOOL(person_name_compare(a,b) <= 0);
 }
+
 
 PG_FUNCTION_INFO_V1(person_name_equal);
 
 Datum
-// equal: person name x == person name y
 person_name_equal(PG_FUNCTION_ARGS)
 {
-	PersonName    *x = (PersonName *) PG_GETARG_POINTER(0);
-	PersonName    *y = (PersonName *) PG_GETARG_POINTER(1);
+	PersonName    *a = (PersonName *) PG_GETARG_POINTER(0);
+	PersonName    *b = (PersonName *) PG_GETARG_POINTER(1);
 
-	PG_RETURN_BOOL(person_name_compare(x, y) == 0);
+	// PG_RETURN_BOOL(strcmp(a->pname, b->pname) == 0);
+	PG_RETURN_BOOL(person_name_compare(a,b) == 0);
 }
 
 PG_FUNCTION_INFO_V1(person_name_greater_than_or_equal);
 
 Datum
-// greater than or equal: person name x >= person name y
 person_name_greater_than_or_equal(PG_FUNCTION_ARGS)
 {
-	PersonName    *x = (PersonName *) PG_GETARG_POINTER(0);
-	PersonName    *y = (PersonName *) PG_GETARG_POINTER(1);
+	PersonName    *a = (PersonName *) PG_GETARG_POINTER(0);
+	PersonName    *b = (PersonName *) PG_GETARG_POINTER(1);
 
-	PG_RETURN_BOOL(person_name_compare(x, y) >= 0);
+	// PG_RETURN_BOOL(strcmp(a->pname, b->pname) == 0);
+	PG_RETURN_BOOL(person_name_compare(a,b) >= 0);
 }
 
 PG_FUNCTION_INFO_V1(person_name_greater_than);
 
 Datum
-// greater than: person name x > person name y
 person_name_greater_than(PG_FUNCTION_ARGS)
 {
-	PersonName    *x = (PersonName *) PG_GETARG_POINTER(0);
-	PersonName    *y = (PersonName *) PG_GETARG_POINTER(1);
+	PersonName    *a = (PersonName *) PG_GETARG_POINTER(0);
+	PersonName    *b = (PersonName *) PG_GETARG_POINTER(1);
 
-	PG_RETURN_BOOL(person_name_compare(x, y) > 0);
+	// PG_RETURN_BOOL(strcmp(a->pname, b->pname) == 0);
+	PG_RETURN_BOOL(person_name_compare(a,b) > 0);
 }
 
-/*****************************************************************************
- * Support Functions
- *
- * person_name_compare
- * 
- *****************************************************************************/
-
-// PG_FUNCTION_INFO_V1(person_name_compare);
+// PG_FUNCTION_INFO_V1(complex_abs_cmp);
 
 // Datum
-// // compare: person name x and person name y
-// person_name_compare(PG_FUNCTION_ARGS)
+// complex_abs_cmp(PG_FUNCTION_ARGS)
 // {
-// 	PersonName    *x = (PersonName *) PG_GETARG_POINTER(0);
-// 	PersonName    *y = (PersonName *) PG_GETARG_POINTER(1);
+// 	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
+// 	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
 
-// 	PG_RETURN_INT32(person_name_compare(x, y));
+// 	PG_RETURN_INT32(complex_abs_cmp_internal(a, b));
 // }
 
 PG_FUNCTION_INFO_V1(family);
 
 Datum
-// return the family part of a name
 family(PG_FUNCTION_ARGS)
 {
 	PersonName    *x = (PersonName *) PG_GETARG_POINTER(0);
-	int len_x = strlen(x->person_name);
-	char *person_name_x = x->person_name;
+
 	int index_x = 0;
-	char *family_name_x;
+	int len_x = strlen(x->person_name);
 	char *result;
 
-	// get the index of comma of the person name x
 	for(int i = 0; i < len_x; i++){
 		if(x->person_name[i] == ','){
 			index_x = i;
 			break;
 		}
 	}
+	
+	x->person_name[index_x] = '\0';
+	result = psprintf("%s", x->person_name);
+	x->person_name[index_x] = ',';
 
-	// get the family name of x
-	strncpy(family_name_x, person_name_x, index_x);
-
-	result = psprintf("%s",family_name_x);
-
-	PG_RETURN_CSSTRING(result);
+	PG_RETURN_CSTRING(result);
 }
 
-PG_FUNCTION_INFO_V1(given);
+// PG_FUNCTION_INFO_V1(given);
+
+// Datum
+// given(PG_FUNCTION_ARGS)
+// {
+// 	PersonName    *x = (PersonName *) PG_GETARG_POINTER(0);
+// 	char * given_name;
+// 	char * result;
+// 	int index_x = 0;
+// 	int len_x = strlen(x->person_name);
+
+// 	for(int i = 0; i < len_x; i++){
+// 		if(x->person_name[i] == ','){
+// 			index_x = i;
+// 			break;
+// 		}
+// 	}
+
+// 	// char * a_given_name = strchr(a->pname, ',') + 1;
+
+// 	if(*(given_name) == ' '){
+// 		given_name++;
+// 	}
+	
+// 	result = psprintf("%s", given_name);
+
+// 	PG_RETURN_SCTRING(result);
+// }
+
+// PG_FUNCTION_INFO_V1(show);
+
+
+// Datum
+// // return the family part of a name
+// show(PG_FUNCTION_ARGS)
+// {
+// 	PersonName    *x = (PersonName *) PG_GETARG_POINTER(0);
+	
+// 	// char * family_name;
+// 	char * family_result;
+// 	char * given_name;
+// 	char * given_result;
+// 	char * full_name;
+// 	int index_x = 0;
+// 	int len_x = strlen(x->person_name);
+
+// 	for(int i = 0; i < len_x; i++){
+// 		if(x->person_name[i] == ','){
+// 			index_x = i;
+// 			break;
+// 		}
+// 	}
+
+// 	x->person_name[index_x] = '\0';
+// 	family_result = psprintf("%s", x->person_name);
+// 	x->person_name[index_x] = ',';
+
+// 	if(*(given_name) == ' '){
+// 		given_name++;
+// 	}
+	
+// 	given_result = psprintf("%s", given_name);
+// 	strcpy(full_name, family_result);
+// 	strcat(full_name, given_result);
+
+
+
+// PG_RETURN_CSTRING(full_name);
+// }
+PG_FUNCTION_INFO_V1(pname_hash);
 
 Datum
-// return the given part of a name
-given(PG_FUNCTION_ARGS)
+pname_hash(PG_FUNCTION_ARGS)
 {
-	PersonName    *x = (PersonName *) PG_GETARG_POINTER(0);
-
-	int len_x = strlen(x->person_name);
-
-	char *person_name_x = x->person_name;
-
+	PersonName  *x = (PersonName *) PG_GETARG_POINTER(0);
+	// delete space after comma
+	int hash_code = 0;
 	int index_x = 0;
-
-	// get the index of comma of the person name x
+	int len_x = strlen(x->person_name);
 	for(int i = 0; i < len_x; i++){
 		if(x->person_name[i] == ','){
 			index_x = i;
@@ -383,122 +431,12 @@ given(PG_FUNCTION_ARGS)
 		}
 	}
 
-	char *given_name_x;
-
-	// get the given name of x
-	strncpy(given_name_x, person_name_x + index_x + 1, len_x - index_x - 1);
-
-	// if there is a space after comma
-	if(x->person_name[index_x + 1] == ' '){
-		given_name_x++;
-	}
-
-	char *result;
-
-	result = psprintf("%s",given_name_x);
-
-	PG_RETURN_CSSTRING(result);
-}
-
-PG_FUNCTION_INFO_V1(show);
-
-Datum
-// return the family part of a name
-show(PG_FUNCTION_ARGS)
-{
-	PersonName    *x = (PersonName *) PG_GETARG_POINTER(0);
-
-	int len_x = strlen(x->person_name);
-
-	char *person_name_x = x->person_name;
-
-	int index_x = 0;
-
-	// get the index of comma of the person name x
-	for(int i = 0; i < len_x; i++){
-		if(x->person_name[i] == ','){
-			index_x = i;
-			break;
-		}
-	}
-
-	char *family_name_x;
-	// get the family name of x
-	strncpy(family_name_x, person_name_x, index_x);
-
-	char *given_name_x;
-	// get the given name of x
-	strncpy(given_name_x, person_name_x + index_x + 1, len_x - index_x - 1);
-
-	// if there is a space after comma
-	if(x->person_name[index_x + 1] == ' '){
-		given_name_x++;
-	}
-
-	for(int j = 0; j < strlen(given_name_x); j++){
-		if(given_name_x[j] == ' '){
-			given_name_x[j] = '/0';
-			break; 
-		}
-	}
-
-	//+2  1 for the zero-terminator, 1 for the space in the middle
-    char *full_name = malloc(strlen(family_name_x)+strlen(given_name_x)+2);
-
-	if (full_name == NULL){ 
-		exit (1);
-	}
-
-	// combine given_name, space and family_name
-	// e.g. Given Family
-    strcpy(full_name, given_name_x);
-
-    // strcat(full_name, space);
-	strcat(full_name, family_name_x);
-
-	char *result;
-
-	result = psprintf("%s",full_name);
-
-	PG_RETURN_CSSTRING(result);
-}
-
-PG_FUNCTION_INFO_V1(person_name_hash);
-
-Datum
-person_name_hash(PG_FUNCTION_ARGS){
-
-	PersonName    *x = (PersonName *) PG_GETARG_POINTER(0);
-
-	int hash_number = 0;
-
-	int len_x = strlen(x->person_name);
-
-	int index_x = 0;
-
-	// get the index of comma of the person name x
-	for(int i = 0; i < len_x; i++){
-		if(x->person_name[i] == ','){
-			index_x = i;
-			break;
-		}
-	}
-
-	char *person_name_x = x->person_name;
-
-	///delete space after comma
-	// if(x->person_name[index_x + 1] == ' '){
-
-	// 	char *tmp_a = x->person_name;
-
-	// 	for(int j = 0; j < len_x; j++){
-	// 		if( j != index_x + 1){
-	// 			*tmp_a++ = *person_name_x;
-	// 		}
-	// 		person_name_x++;
-	// 	}
-	// 	*tmp_a =  '/0';
+	// if (x->person_name[index_x + 1] == ' '){
+	// 	x->person_name[index_x + 1] == '';
 	// }
-	int hash_number = DatumGetUInt32(hash_any((unsigned char *) person_name_x, len_x));
-	PG_RETURN_INT32(hash_number); 
+
+
+	hash_code = DatumGetUInt32(hash_any((unsigned char *)x->person_name, strlen(x->person_name)));
+
+	PG_RETURN_INT32(hash_code);
 }
