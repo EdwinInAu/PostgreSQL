bb5b5adf12d822194c939ed1dbba1331bc2bd160
Edwin <56682784+EdwinInAu@users.noreply.github.com>
2020-03-20 12:53:16 +1100

    Update pname.c

--- pname.c
+++ pname.c
@@ -287,7 +287,7 @@ PG_FUNCTION_INFO_V1(peson_name_less_than_or_equal);
 
 Datum
 // less than or equal: person name x <= person name y
-peson_name_less_than_or_equal(PG_FUNCTION_ARGS)
+person_name_less_than_or_equal(PG_FUNCTION_ARGS)
 {
 	PersonName    *x = (PersonName *) PG_GETARG_POINTER(0);
 	PersonName    *y = (PersonName *) PG_GETARG_POINTER(1);
