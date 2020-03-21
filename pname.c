5d7c602d6cd543a6a54e5cf1e7e288c9e1adf4ec
Alan <62050854+alannan55@users.noreply.github.com>
2020-03-22 00:24:14 +1100

    Add files via upload
    Smith, John A

--- pname.c
+++ pname.c
@@ -2,9 +2,9 @@
  * src/tutorial/pname.c
  *
  ******************************************************************************
-  This file contains routines that can be bound to a Postgres backend and
-  called by the backend in the process of processing queries.  The calling
-  format for these routines is dictated by Postgres architecture.
+	This file contains routines that can be bound to a Postgres backend and
+	called by the backend in the process of processing queries.  The calling
+	format for these routines is dictated by Postgres architecture.
 ******************************************************************************/
 
 #include "postgres.h"
@@ -30,6 +30,7 @@ char* delete(char *x);
 int check_input(char *string);
 int person_name_compare(PersonName *x, PersonName *y);
 
+// check the valid person name format
 int check_input(char *string){
 
 	int len = strlen(string);
@@ -47,13 +48,33 @@ int check_input(char *string){
 			comma_number++;
 			index = i;
 		}
+		// if there is one Letter at 1st place
+		if(string[i] == ' ' && i == 1 && isalpha(string[0])){
+			return 0;
+		}
+		// if there is one letter between 2 spaces
+		if(string[i] == ' ' && i > 1 && i+2 < len && string[i+2] == ' '){
+			return 0;
+		}
+		// if there is one letter between a comma and a space
+		if(string[i] == ' ' && i > 1 && i > index && index > 0 && i-index == 2){
+			return 0;
+		}
+		// if there is one letter between a space and a comma
+		if(string[i] == ' ' && i > 1 && i < index && index - i == 2){
+			return 0;
+		}
+		// if there is one letter after a space in the end
+		if(string[i] == ' ' && i > 1 && len - i == 2){
+			return 0;
+		}
 		if(string[i] != ' ' && string[i] != ',' && string[i] != '-' && string[i] != '\'' && !isalpha(string[i])){
 			return 0;
 		}
 		if(isdigit(string[i])){
 			return 0;
 		}
-		// after a space or "-", there is no upper-case letter
+		// after a space or "-", there is lower-case letter
 		if(string[i] == ' ' || string[i] == '-'){
 			if(isalpha(string[i+1]) && !isupper(string[i+1])){
 				return 0;
@@ -95,7 +116,7 @@ int check_input(char *string){
 	if(string[len-1] == ' '){
 		return 0;
 	}
-	// if the string includes "Dr", "Prof","Mr" or "Ms
+	// if the string includes "Dr.", "Prof.","Mr." or "Ms."
 	if((strstr(string,title_a)!=NULL)||(strstr(string,title_b)!=NULL)||(strstr(string,title_c) != NULL)||(strstr(string,title_d) != NULL)){
 		return 0;
 	}
@@ -106,6 +127,7 @@ int check_input(char *string){
 	return 1;
 }
 
+// delete the space between family name and given name
 char* delete(char *x)
 {
 	int len = strlen(x);
@@ -114,6 +136,7 @@ char* delete(char *x)
 	char *family_name;
 	char *given_name;
 	char *full_name;
+
 	// get the index of comma of the person name x
 	for(int i = 0; i < len; i++){
 		if(x[i] == ','){
@@ -127,14 +150,12 @@ char* delete(char *x)
 		exit (1);
 	}
 
-	// get the family name of x
+	// get the family name of x and comma
 	strncpy(family_name, x, index+1);
-
 	family_name[index+1] = '\0';
 
-    // get tge given name of x
+    // get the given name of x
     given_name = strchr(x, ',');
-
 	given_name++;
 
 	// if there is a space after comma
@@ -151,6 +172,10 @@ char* delete(char *x)
 	// e.g. Family,Given
     strcpy(full_name, family_name);
 	strcat(full_name, given_name);
+	
+	// free memory
+	pfree(family_name);
+
     return full_name;
 }
 /*****************************************************************************
@@ -166,7 +191,8 @@ pname_in(PG_FUNCTION_ARGS)
 	PersonName *result;
 
 	int len = strlen(str);
-	char *new;
+	char *new_str;
+
 	// check valid person name format
 	if(check_input(str) == 0){
 		ereport(ERROR,
@@ -174,11 +200,14 @@ pname_in(PG_FUNCTION_ARGS)
 				errmsg("invalid input syntax for type %s: \"%s\"",
 						"PersonName", str)));
 	}
-	new = delete(str);
+	// delete space between family name and given name
+	// e.g. Family,Given
+	new_str = delete(str);
+
 	result = (PersonName *) palloc(len + 1 +VARHDRSZ);
 	SET_VARSIZE(result , len + 1 +VARHDRSZ);
 	// assign the value of str to the result
-	snprintf(result->person_name,len+1,"%s",new);
+	snprintf(result->person_name,len+1,"%s",new_str);
 
 	PG_RETURN_POINTER(result);
 }
@@ -243,9 +272,7 @@ pname_send(PG_FUNCTION_ARGS)
  * 
  *****************************************************************************/
 
-// compare two person names
-// equal: return 0
-// else: return strcmp result
+// support function: compare two person names
 PG_FUNCTION_INFO_V1(person_name_cmp);
 Datum
 person_name_cmp(PG_FUNCTION_ARGS)
@@ -256,6 +283,9 @@ person_name_cmp(PG_FUNCTION_ARGS)
 	PG_RETURN_INT32(person_name_compare(a,b));
 }
 
+// compare two person names
+// equal: return 0
+// else: return strcmp result
 int person_name_compare(PersonName *x, PersonName *y){
 
 	int len_x = strlen(x->person_name);
@@ -304,6 +334,10 @@ int person_name_compare(PersonName *x, PersonName *y){
 
     // compare family name x and y
 	family_name_result = strcmp(family_name_x,family_name_y);
+
+	// free the memory
+	pfree(family_name_x);
+	pfree(family_name_y);
 
 	// judge the family name
 	if(family_name_result != 0){
@@ -413,6 +447,7 @@ person_name_unequal(PG_FUNCTION_ARGS)
  * family
  * given
  * show
+ * hash
  * 
  *****************************************************************************/
 
@@ -541,12 +576,14 @@ show(PG_FUNCTION_ARGS)
 		exit (1);
 	}
 
+	// find the index of space within the given name
 	for(int j = 0; j < strlen(given_name); j++){
 		if(given_name[j] == ' '){
             space_index = j;
 			break; 
 		}
 	}
+	// only get the single given name before the space
     if(space_index > 0){
         strncpy(tmp, given_name, space_index);
         tmp[space_index] = '\0';
@@ -560,76 +597,34 @@ show(PG_FUNCTION_ARGS)
 		exit (1);
 	}
 
-	// combine given_name, space and family_name
+	// combine given_name(single), space and family_name
 	// e.g. Given Family
     strcpy(full_name, tmp);
     strcat(full_name, " ");
 	strcat(full_name, family_name);
+	
+	// free memory
+	pfree(family_name);
+	pfree(tmp);
+
 	PG_RETURN_CSTRING(cstring_to_text(full_name));
 }
 
 PG_FUNCTION_INFO_V1(person_name_hash);
-
+// hash function: index
 Datum
 person_name_hash(PG_FUNCTION_ARGS){
+	
 	PersonName    *x = (PersonName *) PG_GETARG_POINTER(0);
 
-	int len_x = strlen(x->person_name);
-
-	int index = 0;
-
-    char *family_name;
-    char *given_name;
-    char *full_name;
 	int hash_number;
+	char *result;
 
-	// get the index of comma of the person name x
-	for(int i = 0; i < len_x; i++){
-		if(x->person_name[i] == ','){
-			index = i;
-			break;
-		}
-	}
+	int len = strlen(x->person_name) + 1;
 
-    family_name = (char*) palloc(sizeof(char) * (index+2));
+	result = x->person_name;
 
-    if (family_name == NULL){ 
-		exit (1);
-	}
-
-	// get the family name of x
-	strncpy(family_name, x->person_name, index);
-
-	family_name[index] = '\0';
-
-	// get the given name of x
-    given_name = strchr(x->person_name, ',');
-
-	given_name++;
-
-	// if there is a space after comma
-	if(x->person_name[index + 1] == ' '){
-		given_name++;
-	}
-
-	//+2  1 for the zero-terminator, 1 for the space in the middle
-    full_name = (char*) palloc(sizeof(char *)*(strlen(family_name)+strlen(given_name)+2));
-    if (full_name == NULL){ 
-		exit (1);
-	}
-
-	// combine given_name, space and family_name
-	// e.g. Given Family
-    strcpy(full_name, family_name);
-    strcat(full_name, ",");
-	strcat(full_name, given_name);
-
-
-	// int hash_number;
-	// char *result;
-	// int len = strlen(x->person_name) + 1;
-	// result = x->person_name;
-	hash_number = DatumGetUInt32(hash_any((const unsigned char *) full_name, strlen(full_name) + 2 ));
-	// PG_RETURN_INT32(hash_number); 
-	PG_RETURN_DATUM(hash_number);
+	hash_number = DatumGetUInt32(hash_any((const unsigned char *) result, len));
+	
+	PG_RETURN_INT32(hash_number); 
 }
