4b221555741ef7ab41b7f58988165371cf9c09da
Edwin <wcs2019inau@gmail.com>
2020-04-22 19:16:46 +1000

    step5 first 1

--- Project2/bsig.c
+++ Project2/bsig.c
@@ -6,11 +6,37 @@
 #include "reln.h"
 #include "query.h"
 #include "bsig.h"
+#include "psig.h"
 
 void findPagesUsingBitSlices(Query q)
 {
 	assert(q != NULL);
 	//TODO
-	setAllBits(q->pages); // remove this
+	Reln relation = q->rel;
+	File bitSignatureFile = bsigFile(relation);
+    Bits queryPageSignature = makePageSig(relation, q->qstring);
+    Count maxBitSlicesPP = maxBsigsPP(relation);
+    Count pm = psigBits(relation);
+	Count bm = bsigBits(relation);
+	Bits pages = newBits(bm);
+    setAllBits(pages);
+    int pageInit = -10;
+    for(int index = 0; index < pm; idex++){
+        if(bitIsSet(queryPageSignature, index) == TRUE) {
+            int pageID = index / maxBitSlicesPP;
+            if(pageID != pageInit){
+                pageInit = pageID;
+                q->nsigpages++;
+            }
+            Page currentPage = getPage(bitSignatureFile, pageID);
+            Bits tmp = newBits(bm);
+            Offset position = index % maxBitSlicesPP;
+            getBits(currentPage, position, tmp);
+            andBits(pages, tmp);
+            freeBits(tmp);
+            q->nsigs++;
+        }
+    }
+    // 好像还缺个q->pages
 }
 
